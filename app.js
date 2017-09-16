5bcfd7870f910e9b4ade101797b5b9728bfa6bbf
Hussain Abbas <habbas@keyhole.co>
2017-09-16 16:46:33 -0400

    express coinbase oauth

---
+++ app.js
@@ -0,0 +1,233 @@
+'use strict';
+
+var express = require('express'),
+  app = express(),
+  bodyParser = require("body-parser"), //L.R.
+  errorhandler = require('errorhandler'),
+  session = require('express-session'),
+  bluemix = require('./config/bluemix'),
+  watson = require('watson-developer-cloud'),
+  path = require('path'),
+  passport = require('passport'),
+  CoinbaseStrategy = require('passport-coinbase').Strategy,
+  // environmental variable points to demo's json config file
+  extend = require('util')._extend,
+  fs = require('fs');
+
+// Rename .html to ejs
+fs.rename(__dirname + '/dist/index.html', __dirname + '/dist/index.ejs', function (err) {
+  if (err) {
+    fs.stat(__dirname + '/dist/index.ejs', function (err) {
+      if (err) return console.warn('ERROR:' + err);
+    })
+  }
+  console.log("index.html: HTML to EJS success!");
+});
+
+// For local development, put username and password in config
+// or store in your environment
+var config = {
+  version: 'v1',
+  url: 'https://stream.watsonplatform.net/speech-to-text/api',
+  username: "676a5bd9-8cd8-4d79-8a6c-8790137934d6",
+  password: "P0Lz3XYep07Q"
+};
+
+// if bluemix credentials exists, then override local
+var credentials = extend(config, bluemix.getServiceCreds('speech_to_text'));
+var authorization = watson.authorization(credentials);
+
+// Coinbase api token
+var COINBASE_CLIENT_ID = "89471a61cd057bc083ae8b2aa3e131b896682013c25760e3c6e115352049f9db";
+var COINBASE_CLIENT_SECRET = "e96067050d0a0f966833bd89770d0c45bdb59ef18a259012fdf79058315a3fe7";
+
+// Passport session setup.
+//   To support persistent login sessions, Passport needs to be able to
+//   serialize users into and deserialize users out of the session.  Typically,
+//   this will be as simple as storing the user ID when serializing, and finding
+//   the user by ID when deserializing.  However, since this example does not
+//   have a database of user records, the complete Coinbase profile is serialized
+//   and deserialized.
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+
+
+// Use the CoinbaseStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken and refreshToken),
+//   and invoke a callback with a user object.
+passport.use(new CoinbaseStrategy({
+  clientID: COINBASE_CLIENT_ID,
+  clientSecret: COINBASE_CLIENT_SECRET,
+  callbackURL: "http://127.0.0.1:5000/auth/coinbase/callback",
+  scope: ["user"]
+},
+  function (accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+
+      // To keep the example simple, the user's Coinbase profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the Coinbase account with a user record in your database,
+      // and return that user instead.
+      return done(null, profile);
+    });
+  }
+));
+
+// configure Express
+app.set('views', __dirname + '/dist');
+app.set('view engine', 'ejs');
+// app.use(express.bodyParser());
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(session({ secret: 'keyboard cat' }));
+// Initialize Passport!  Also use passport.session() middleware, to support
+// persistent login sessions (recommended).
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(__dirname + '/dist'));
+
+
+// redirect to https if the app is not running locally
+if (!!process.env.VCAP_SERVICES) {
+  app.enable('trust proxy');
+  app.use(function (req, res, next) {
+    if (req.secure) {
+      next();
+    } else {
+      res.redirect('https://' + req.headers.host + req.url);
+    }
+  });
+}
+
+app.get('/', function (req, res) {
+  res.render('index', { user: req.user });
+});
+
+app.get('/account', ensureAuthenticated, function (req, res) {
+  res.render('account', { user: req.user });
+});
+
+app.get('/login', function (req, res) {
+  res.render('login', { user: req.user });
+});
+
+// GET /auth/coinbase
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in Coinbase authentication will involve redirecting
+//   the user to coinbase.com.  After authorization, Coinbase will redirect the user
+//   back to this application at /auth/coinbase/callback
+app.get('/auth/coinbase',
+  passport.authenticate('coinbase'),
+  function (req, res) {
+    // The request will be redirected to Coinbase for authentication, so this
+    // function will not be called.
+  });
+
+// GET /auth/coinbase/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function function will be called,
+//   which, in this example, will redirect the user to the home page.
+app.get('/auth/coinbase/callback',
+  passport.authenticate('coinbase', { failureRedirect: '/login' }),
+  function (req, res) {
+    console.log(JSON.stringify(req.user));
+    res.redirect('/');
+  });
+
+app.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+// Get token from Watson using your credentials
+app.get('/token', function (req, res) {
+  authorization.getToken({
+    url: credentials.url
+  }, function (err, token) {
+    if (err) {
+      console.log('error:', err);
+      res.status(err.code);
+    }
+    res.send(token);
+  });
+});
+
+var mt_credentials = extend({
+  url: 'https://gateway.watsonplatform.net/language-translation/api',
+  username: "4b2ee4f8-03c6-4637-a04c-a3e170999a9a",
+  password: "qMbwHMdULtBm",
+  version: 'v2'
+}, bluemix.getServiceCreds('language-translation')); // VCAP_SERVICES
+
+var language_translation = watson.language_translation(mt_credentials);
+
+app.post('/api/translate', function (req, res, next) {
+  //console.log('/v2/translate');
+
+  var params = extend({
+    'X-WDC-PL-OPT-OUT': req.header('X-WDC-PL-OPT-OUT')
+  }, req.body);
+  //console.log(' ---> params == ' + JSON.stringify(params)); //L.R.
+
+  language_translation.translate(params, function (err, models) {
+    if (err)
+      return next(err);
+    else
+      res.json(models);
+  });
+});
+// ----------------------------------------------------------------------
+
+// L.R.
+// -------------------------------- TTS ---------------------------------
+var tts_credentials = extend({
+  url: 'https://stream.watsonplatform.net/text-to-speech/api',
+  version: 'v1',
+  username: "bc632dc3-e824-4978-91be-ba768307db40",
+  password: "z8ZupzmYPKY2"
+}, bluemix.getServiceCreds('text_to_speech'));
+
+// Create the service wrappers
+var textToSpeech = watson.text_to_speech(tts_credentials);
+
+app.get('/synthesize', function (req, res) {
+  var transcript = textToSpeech.synthesize(req.query);
+  transcript.on('response', function (response) {
+    if (req.query.download) {
+      response.headers['content-disposition'] = 'attachment; filename=transcript.ogg';
+    }
+  });
+  transcript.on('error', function (error) {
+    console.log('Synthesize error: ', error)
+  });
+  transcript.pipe(res);
+});
+
+// ----------------------------------------------------------------------
+
+// Add error handling in dev
+if (!process.env.VCAP_SERVICES) {
+  app.use(errorhandler());
+}
+var port = process.env.VCAP_APP_PORT || 5000;
+app.listen(port);
+console.log('listening at:', port);
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="hello">
-    <h1>BS Chain</h1>
-    <p class="tagline">Lose more than just respect</p>
+    <div class="hero">
+      <h1>
+        <a class="hero__title" href="#">
+          <span class="hero--white">BS</span><span class="hero--yellow">Chain</span>
+        </a>
+      </h1>
+      <p class="tagline">Lose more than just respect</p>
+    </div>
 
     <a href="#">
       <el-button type="primary">Connect Coinbase</el-button>
@@ -10,8 +16,6 @@
 </template>
 
 <script>
-  import { mapActions } from 'vuex'
-
   export default {
     name: 'hello'
   }
@@ -19,9 +23,77 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="stylus" scoped>
-  $green = #41b883
-  $dark-blue = #35495e
+  $white = #fff
+  @font-face
+    font-family: 'adam.cg_proregular';
+    src: url("/static/fonts/adam.cg_pro-webfont.eot");
+    src: url("/static/fonts/adam.cg_pro-webfont.eot?#iefix") format('embedded-opentype'), url("/static/fonts/adam.cg_pro-webfont.woff2") format('woff2'), url("/static/fonts/adam.cg_pro-webfont.woff") format('woff'), url("/static/fonts/adam.cg_pro-webfont.ttf") format('truetype'), url("/static/fonts/adam.cg_pro-webfont.svg#adam.cg_proregular") format('svg');
+    font-weight: normal;
+    font-style: normal;
+  @font-face
+    font-family: 'againtsregular';
+    src: url("/static/fonts/againts-webfont.woff2") format('woff2'), url("/static/fonts/againts-webfont.woff") format('woff');
+    font-weight: normal;
+    font-style: normal;
 
-  h2
-    font-weight normal
+  a
+    text-decoration none
+
+  h1
+    margin 0
+  
+  .hero
+    padding 5em 1em
+    background-image url(https://images.unsplash.com/photo-1498713301984-508015049dc1?dpr=2&auto=format&fit=crop&w=1199&h=798&q=80&cs=tinysrgb)
+    background-size cover
+    background-repeat no-repeat
+    color $white
+
+  .hero__title
+    font-size: 7.5em;
+    border-bottom: 0;
+    font-weight: 100;
+    letter-spacing: 2px;
+    font-family: 'againtsregular';
+    position: relative;
+    color: #fff;
+    text-shadow: 2px 2px 2px rgba(0,0,0,0.2), 0 0 10px rgba(0,0,0,0.1);
+    z-index: 1;
+    display: inline-block;
+    animation-name: floating;
+    animation-duration: 10s;
+    animation-iteration-count: infinite;
+    animation-timing-function: ease-in-out;
+    &:before
+      z-index: -1;
+      content: '_';
+      position: absolute;
+      color: rgba($white, 0.2);
+      left: 0;
+      transform: translateX(-15%);
+      font-size: 35rem;
+      line-height: 20px;
+
+  .hero--yellow
+    color: #ffc600;
+    position: relative;
+    top: 0.07em;
+
+  .tagline
+    font-size 1em
+    font-family: 'Roboto';
+    text-transform uppercase
+    font-weight 400
+    letter-spacing 6px
+
+@keyframes floating
+  from
+    transform: translate(0, 0px) rotateY(-12deg);
+    text-shadow: 10px 10px 3px rgba(0,0,0,0.15);
+  65%
+    transform: translate(0, 15px) rotateY(10deg) rotateX(10deg);
+    text-shadow: -10px -10px 3px rgba(0,0,0,0.15);
+  to
+    transform: translate(0, 0px) rotateY(-12deg);
+    text-shadow: 10px 10px 3px rgba(0,0,0,0.15);
 </style>
